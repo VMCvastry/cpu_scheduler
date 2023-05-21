ea61e884ea974f0c70fc8d23c53a783c59bf3e9c
vmcvastry <009valerio@gmail.com>
2023-05-21 17:52:31 +0200

    SJF prediction

--- schedulers.c
+++ schedulers.c
@@ -22,7 +22,6 @@ void schedRR(FakeOS *os, void *args_)
     e->burst_time = args->quantum;
 };
 void schedSJF(FakeOS *os, void *args_)
-
 {
     SchedRRArgs *args = (SchedRRArgs *)args_;
     // look for the first process in ready
@@ -52,6 +51,35 @@ void schedSJF(FakeOS *os, void *args_)
 
     e->burst_time = args->quantum;
 };
+static const float a = 0.5;
+void schedSJF_PRED(FakeOS *os, void *args_)
+{
+    SchedRRArgs *args = (SchedRRArgs *)args_;
+    if (!os->ready.first)
+        return;
+
+    FakePCB *best_pcb = (FakePCB *)os->ready.first;
+    for (ListItem *aux = os->ready.first; aux; aux = aux->next)
+    {
+        FakePCB *pcb = (FakePCB *)aux;
+        if (pcb->predicted_burst_duration < best_pcb->predicted_burst_duration)
+        {
+            fprintf(stderr, "swi\n");
+            best_pcb = pcb;
+        }
+    }
+
+    List_detach(&os->ready, (ListItem *)best_pcb);
+    os->running = best_pcb;
+    assert(best_pcb->events.first);
+    ProcessEvent *e = (ProcessEvent *)best_pcb->events.first;
+    assert(e->type == CPU);
+    e->burst_time = args->quantum;
+    if (e->duration <= args->quantum) // Last time this event will run
+    {
+        best_pcb->predicted_burst_duration = a * e->total_duration + (1 - a) * best_pcb->predicted_burst_duration; // Calculate next event prediction
+    }
+};
 
 void (*getSched(Sched s))(FakeOS *os, void *args_)
 {
@@ -63,6 +91,9 @@ void (*getSched(Sched s))(FakeOS *os, void *args_)
     case SJF:
         fprintf(stderr, "SJF\n");
         return schedSJF;
+    case SJF_PRED:
+        fprintf(stderr, "SJF_PRED\n");
+        return schedSJF_PRED;
     default:
         assert(0);
     }
