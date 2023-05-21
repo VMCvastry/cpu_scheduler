66a042280f29f1da0e3e10edac199b7d58b41598
vmcvastry <009valerio@gmail.com>
2023-05-21 16:54:16 +0200

    sjf

--- schedulers.c
+++ schedulers.c
@@ -34,7 +34,49 @@ void schedRR(FakeOS *os, void *args_)
         List_pushFront(&pcb->events, (ListItem *)qe);
     }
 };
-#elif SCHED == SJF
+void schedSJF(FakeOS *os, void *args_)
+
+{
+    SchedRRArgs *args = (SchedRRArgs *)args_;
+    // look for the first process in ready
+    // if none, return
+    if (!os->ready.first)
+        return;
+
+    FakePCB *best_pcb = (FakePCB *)os->ready.first;
+    ProcessEvent *best_e = (ProcessEvent *)best_pcb->events.first;
+    for (ListItem *aux = os->ready.first; aux; aux = aux->next)
+    {
+        FakePCB *pcb = (FakePCB *)aux;
+        ProcessEvent *e = (ProcessEvent *)pcb->events.first;
+        if (e->duration < best_e->duration)
+        {
+            fprintf(stderr, "swi\n");
+
+            best_pcb = pcb;
+            best_e = (ProcessEvent *)best_pcb->events.first;
+        }
+    }
+    List_detach(&os->ready, (ListItem *)best_pcb);
+    os->running = best_pcb;
+    assert(best_pcb->events.first);
+    ProcessEvent *e = (ProcessEvent *)best_pcb->events.first;
+    assert(e->type == CPU);
+
+    // look at the first event
+    // if duration>quantum
+    // push front in the list of event a CPU event of duration quantum
+    // alter the duration of the old event subtracting quantum
+    if (e->duration > args->quantum)
+    {
+        ProcessEvent *qe = (ProcessEvent *)malloc(sizeof(ProcessEvent));
+        qe->list.prev = qe->list.next = 0;
+        qe->type = CPU;
+        qe->duration = args->quantum;
+        e->duration -= args->quantum;
+        List_pushFront(&best_pcb->events, (ListItem *)qe);
+    }
+};
 
 void (*getSched(Sched s))(FakeOS *os, void *args_)
 {
